46ffd1a8db009154ce65cc9c9a1e53ebbc81e4b6
Lapis <88260244+LapisPhoenix@users.noreply.github.com>
2023-03-30 20:18:28 -0700

    ✨Added Logging
    ✨Added logging for the following events:
    1. Banning/Kicking
    2. Editing/Deleting Messages
    3. Joining/Switching/Leaving Voice Channels
    
    ✨Added Levels to Console Logging
    
    🔥 Removed handling Folder
    🔥 Removed events.ts

--- src/commands/ban.ts
+++ src/commands/ban.ts
@@ -1,32 +1,46 @@
 import {
-    ChatInputCommandInteraction,
-    SlashCommandBuilder,
-    PermissionFlagsBits,
-  } from "discord.js";
-  
-  export default {
-    data: new SlashCommandBuilder()
-      .setName("ban")
-      .setDescription(
-        "Permanently ban a user",
-      )
-      .addMentionableOption((option) => {
-        return option.setDescription("User to ban").setName("user").setRequired(true)
-      })
-      .addStringOption((option) => {
-        return option.setDescription("Reason").setName("reason").setRequired(false);
-      })
-      .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
-  
-    async execute(interaction: ChatInputCommandInteraction) {
-      const target = interaction.options.getUser("user")
-      const reason = interaction.options.getString("reason") ?? 'no reason provided.'
-    
-      await interaction.guild.members.ban(target, {
-        reason
-      });
-    
-      await interaction.reply(`Banned ${target.username}#${target.tag}.\nReason: ${reason}`)
-      
-    },
+  ChatInputCommandInteraction,
+  SlashCommandBuilder,
+  PermissionFlagsBits,
+  TextChannel,
+} from "discord.js";
+
+import { logDiscordEvent, logChannelID } from "../lib/logging";
+
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("ban")
+    .setDescription(
+      "Permanently ban a user",
+    )
+    .addMentionableOption((option) => {
+      return option.setDescription("User to ban").setName("user").setRequired(true);
+    })
+    .addStringOption((option) => {
+      return option.setDescription("Reason").setName("reason").setRequired(false);
+    })
+    .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
+
+  async execute(interaction: ChatInputCommandInteraction) {
+    const target = interaction.options.getUser("user")
+    const reason = interaction.options.getString("reason") ?? 'no reason provided.'
+
+    await interaction.guild.members.ban(target, {
+      reason
+    });
+
+    await interaction.reply(`Banned ${target.username}#${target.tag}.\nReason: ${reason}`);
+
+    const logChannel = interaction.client.channels.cache.get(logChannelID) as TextChannel;
+
+    let embed = logDiscordEvent(`Banned ${target.username}#${target.tag}`);
+
+    embed.addFields(
+      { name: 'Reason', value: reason },
+    );
+
+    await logChannel.send({ embeds: [embed] });
+
+  },
 }
