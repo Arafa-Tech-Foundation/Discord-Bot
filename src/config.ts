f5a3ac99a6ed8a37b23336ed594ccfe1134dc279
lmaosoggypancakes <macerajosef@gmail.com>
2023-05-13 14:32:57 -0700

    🔧 add correct config channel IDs

--- src/config.ts
+++ src/config.ts
@@ -1,29 +1,24 @@
 // Make sure to fill in the values needed!
 
-
 // Client //
 export const prefix = process.env.PREFIX;
-export const botID = process.env.CLIENT_ID;  // Why not?
-
+export const botID = process.env.CLIENT_ID; // Why not?
 
 // Server //
 export const guildID: string = process.env.GUILD_ID;
 
 // Channels //
 
-
 // Welcome
-export const welcomeChannelID: string = "012345679";
-
+export const welcomeChannelID: string = "1103905529849466954";
 
 // Log
-export const logChannelID: string = "012345679";
+export const logChannelID: string = "1072404003108962314";
 
 // Starboard
 export const starCount = 5;
-export const starboardChannelID: string = "012345679"
-export const blacklistedStarboardChannelID: string = "012345679"  // Separate with commas
-
+export const starboardChannelID: string = "1093805675399888928";
+export const blacklistedStarboardChannelID: string = "1072403994804244550"; // Separate with commas
 
 // Counting
-export const countChannelID: string = "012345679";
+export const countChannelID: string = "1073496321090138124";
